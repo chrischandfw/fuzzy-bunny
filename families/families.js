69c41653591a1837d3ae097f4e0e16c3b239ef79
chris_chan <chrischandfw@gmail.com>
2022-03-30 17:26:28 -0500

    create elements for family , andme , and bunny

--- families/families.js
+++ families/families.js
@@ -17,7 +17,10 @@ function displayFamilies() {
 
     for (let family of families) {
         // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
-        // your HTML Element should look like this:
+        const famDiv = document.createElement('div');
+		const nameH3= document.createElement('h3');
+		const bunDiv = document.createElement('div');
+		// your HTML Element should look like this:
         // <div class="family">
         //    <h3>the Garcia family</h3>
         //    <div class="bunnies">
@@ -26,7 +29,8 @@ function displayFamilies() {
         //    </div>
         // </div>
         // add the bunnies css class to the bunnies el, and family css class to the family el
-        // put the family name in the name element
+        
+		// put the family name in the name element
         // for each of this family's bunnies
         //    make an element with the css class 'bunny', and put the bunny's name in the text content
         //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
