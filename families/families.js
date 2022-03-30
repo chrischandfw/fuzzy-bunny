44d0cd7ce7bb4802dd7d1732b5218c2a84e3071b
chris_chan <chrischandfw@gmail.com>
2022-03-30 18:12:43 -0500

    completed async display function in families js file

--- families/families.js
+++ families/families.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import { checkAuth, deleteBunny, getFamilies, logout } from '../fetch-utils.js';
 
 checkAuth();
@@ -9,17 +10,18 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+async function displayFamilies() {
     // fetch families from supabase
-	const families = await getFamilies(); 
+    const families = await getFamilies(); 
     // clear out the familiesEl
-	familiesEl.textContent = '';
+    familiesEl.textContent = '';
 
     for (let family of families) {
         // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
         const famDiv = document.createElement('div');
-		const nameH3= document.createElement('h3');
-		const bunDiv = document.createElement('div');
+        const nameH3 = document.createElement('h3');
+        const bunDiv = document.createElement('div');
+
 		// your HTML Element should look like this:
         // <div class="family">
         //    <h3>the Garcia family</h3>
@@ -29,9 +31,26 @@ function displayFamilies() {
         //    </div>
         // </div>
         // add the bunnies css class to the bunnies el, and family css class to the family el
-        
+        famDiv.classList.add('family');
+        bunDiv.classList.add('bunnies');
 		// put the family name in the name element
+        nameH3.textContent = family.name;
         // for each of this family's bunnies
+        for (let bunny of family.fuzzy_bunnies) {
+            const bunEl = document.createElement('div');
+            
+            bunEl.classList.add('bunny');
+            bunEl.textContent = bunny.name;
+			
+            bunEl.addEventListener('click', async () => {
+                await deleteBunny(bunny.id);
+                displayFamilies();
+            });
+
+            bunDiv.append(bunEl);
+        }
+		famDiv.append(nameH3, bunDiv);
+        familiesEl.append(famDiv);
         //    make an element with the css class 'bunny', and put the bunny's name in the text content
         //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
         // append this bunnyEl to the bunniesEl
